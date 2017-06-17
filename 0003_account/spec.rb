1f0955e2ddb4297d0c9a4c909debfb4a06a71f24
furu <ba09219@gmail.com>
2017-06-18 00:21:34 +0900

    Solve 0003_account

--- 0003_account/spec.rb
+++ 0003_account/spec.rb
@@ -1,18 +1,26 @@
-
 class Account
+  attr_accessor :amount
 
-  # TODO
+  def initialize(amount)
+    @amount = amount
+  end
+
+  def withdraw(money)
+    self.amount -= money
+    self
+  end
+
+  def transfer(account, money)
+    account.amount += money
+    self.amount -= money
+    self
+  end
 end
 
-
 context 'quiz 0003' do
-
   describe Account do
-
     describe '.new' do
-
       it 'takes as input the initial amount' do
-
         a = Account.new(10_000)
 
         expect(a.class).to eq(Account)
@@ -20,9 +28,7 @@ context 'quiz 0003' do
     end
 
     describe '#amount' do
-
       it 'returns the current amount' do
-
         a = Account.new(10_000)
 
         expect(a.amount).to eq(10_000)
@@ -30,9 +36,7 @@ context 'quiz 0003' do
     end
 
     describe '#withdraw' do
-
       it 'withdraws from the account' do
-
         a = Account.new(10_000)
         a.withdraw(2_000)
 
@@ -40,21 +44,17 @@ context 'quiz 0003' do
       end
 
       it 'returns the account' do
-
         a = Account.new(10_000)
         r = a.withdraw(2_000)
 
         expect(r).to eq(a)
-
         expect(r.class).to eq(Account)
         expect(r.object_id).to eq(a.object_id)
       end
     end
 
     describe '#transfer' do
-
       it 'transfers from this account to another account' do
-
         a0 = Account.new(10_000)
         a1 = Account.new(11_000)
 
@@ -69,4 +69,3 @@ context 'quiz 0003' do
     end
   end
 end
-
