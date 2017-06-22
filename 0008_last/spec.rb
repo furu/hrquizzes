38aad8217985dfca477dc2e491ac6e54cac51ed8
furu <ba09219@gmail.com>
2017-06-23 00:29:47 +0900

    Solve 0008_last

--- 0008_last/spec.rb
+++ 0008_last/spec.rb
@@ -1,16 +1,10 @@
-
 def foo(path)
-
-  # TODO
+  IO.readlines(path).reject { |line| line == "\n" }.last.strip
 end
 
-
 context 'quiz 0008' do
-
   describe 'foo' do
-
     it "returns the last significant line of a file" do
-
       expect(foo(__FILE__)).to eq('# DEADBEEF')
     end
   end
