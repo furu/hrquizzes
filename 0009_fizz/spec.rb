780092b64525a2dc9a553062990874e4f26cf7c7
furu <ba09219@gmail.com>
2017-06-23 23:43:09 +0900

    Solve 0009_fizz

--- 0009_fizz/spec.rb
+++ 0009_fizz/spec.rb
@@ -1,16 +1,20 @@
-
 def foo(a)
-
-  # TODO
+  a.map do |e|
+    if e % 15 == 0
+      'FizzBuzz'
+    elsif e % 3 == 0
+      'Fizz'
+    elsif e % 5 == 0
+      'Buzz'
+    else
+      e.to_s
+    end
+  end
 end
 
-
 context 'quiz 0009' do
-
   describe 'foo' do
-
     it "takes a list of integers returns Fizz, Buzz or the int" do
-
       expect(foo(0..21).to_a).to eq(%w[
         FizzBuzz 1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz
         16 17 Fizz 19 Buzz Fizz
@@ -18,4 +22,3 @@ context 'quiz 0009' do
     end
   end
 end
-
