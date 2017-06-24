61324031a384e76b774717ac82b3dd423cb37b39
furu <ba09219@gmail.com>
2017-06-25 01:53:55 +0900

    Solve 0010_csv

--- 0010_csv/spec.rb
+++ 0010_csv/spec.rb
@@ -1,20 +1,19 @@
-
 def foo(a)
-
-  # TODO
+  a.map { |r|
+    r
+      .map(&:to_s)
+      .map { |f| f.include?('"') ? f.gsub('"', '""') : f }
+      .map { |f| f.match(/(,|\n|")/) ? "\"#{f}\"" : f }
+  }.map { |r| r.join(',') }.join("\n")
 end
 
-
 context 'quiz 0010' do
-
   describe 'foo' do
-
     it "turns an array into a CSV string" do
-
       expect(
         foo [
-          [ 'basset cafe', 'koi naka, hiroshima-shi', 7 ],
-          [ 'shake-hands', 'kamiyachou, hiroshima-shi', 6 ]
+          ['basset cafe', 'koi naka, hiroshima-shi', 7],
+          ["shake-hands", 'kamiyachou, hiroshima-shi', 6]
         ]
       ).to eq(%q{
 basset cafe,"koi naka, hiroshima-shi",7
@@ -23,4 +22,3 @@ shake-hands,"kamiyachou, hiroshima-shi",6
     end
   end
 end
-
